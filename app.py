c8aced880eeb35a169e84b2ed3b3350022096c7d
Hamza Javed <hamzakhan.hk1994@gmail.com>
2025-02-25 01:04:28 +0500

    third

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ if uploaded_file:
 
 st.markdown("---")
 st.markdown(
-    "🔧 Developed by **Muhammad Ghufran** | 💜 Follow me on [GitHub](https://github.com/ghufran29) & [LinkedIn](https://www.linkedin.com/in/ghufran29/) "
+    "🔧 Developed by **Muhammad Ghufran** | 💜 Follow me on [GitHub](https://github.com/hamza49699) & [LinkedIn](https://www.linkedin.com/in/hamza-khan-7472b822b/) "
 )
 st.markdown("© 2025 PDF ↔ Word Converter. All rights reserved.")
